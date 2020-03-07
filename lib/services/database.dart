188e277dadffc4c380094b14fe3ca54a3d6ce968
Lawful24 <tarkanyilacko@gmail.com>
2020-03-07 19:55:35 +0100

    Initiated firestore records (user)

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -2,7 +2,18 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 
 class DatabaseService {
 
+  final String uid;
+  DatabaseService({ this.uid });
+
   // collection reference
   final CollectionReference songCollection = Firestore.instance.collection('Songs');
+
+  Future updateUserData(String name, int requests, String messages) async {
+    return await songCollection.document(uid).setData({
+      'name': name,
+      'requests': requests,
+      'messages': messages,
+    });
+  }
 
 }
