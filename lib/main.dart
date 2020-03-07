d5d36d0c74f2ea05d66c3f21cc27eac668eb0794
Lawful24 <tarkanyilacko@gmail.com>
2020-03-07 18:05:46 +0100

    Provider package, signing out, forms, toggle, register with firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:studio_application/pages/home/wrapper.dart';
 import 'package:studio_application/pages/loading.dart';
-//import 'package:studio_application/pages/login.dart';
+import 'package:studio_application/pages/login.dart';
 import 'package:studio_application/pages/home/home.dart';
 import 'package:studio_application/pages/request.dart';
 import 'package:studio_application/pages/blacklist.dart';
 import 'package:studio_application/pages/feedback.dart';
 import 'package:studio_application/pages/id.dart';
+import 'package:studio_application/services/auth.dart';
+import 'models/user.dart';
 
-void main() => runApp(MaterialApp(
-  home: Wrapper(),
+void main() => runApp(MyApp(
   /*initialRoute: '/',
   routes: {
     '/': (context) => Loading(),
@@ -20,4 +22,15 @@ void main() => runApp(MaterialApp(
     '/contact': (context) => Contact()
   },*/
 ));
-//TODO: android/build.gradle has 4.0.1 in dependencies, might need to revert to 4.3.3
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return StreamProvider<User>.value(
+      value: AuthService().user,
+      child: MaterialApp(
+        home: Wrapper(),
+      ),
+    );
+  }
+}
--- lib/pages/home/wrapper.dart
+++ lib/pages/home/wrapper.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:studio_application/models/user.dart';
 import 'package:studio_application/pages/authenticate/authenticate.dart';
 import 'package:studio_application/pages/home/home.dart';
 
@@ -6,7 +8,13 @@ class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
+    final user = Provider.of<User>(context);
+
     // return either Home or Authenticate widget
-    return Authenticate();
+    if (user == null) {
+      return Authenticate();
+    } else {
+      return Home();
+    }
   }
 }
